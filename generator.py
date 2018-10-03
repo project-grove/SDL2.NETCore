7b937f2f7cff92798193bfdb86d9aeb2b0cf0f31
Adel Vilkov <vilkov.adel@gmail.com>
2018-10-03 20:21:11 +0300

    More work on binding generator script (need type remapping)

--- generator.py
+++ generator.py
@@ -16,7 +16,7 @@ excluded = list(map(re.compile, [
 excluded.extend(map(lambda n: re.compile("SDL_" + n + ".h"), [
     "assert", "atomic", "bits", "config", "endian", "filesystem", "loadso",
     "log", "main", "mutex", "name", "platform", "quit", "revision", "rwops",
-    "stdinc", "system", "syswm", "thread", "types", 
+    "shape", "stdinc", "system", "syswm", "thread", "types", 
 ]))
 
 def useful_names(name):
@@ -34,9 +34,73 @@ for (dirpath, dirnames, filenames) in walk(header_path):
 header_filenames = filter(useful_names, header_filenames)
 headers = map(lambda f: (f, os.path.join(header_path, f)), header_filenames)
 
-STRUCT_REGEX = re.compile(r"(?<=typedef struct)(?:[\s\w]*)\{(?P<contents>[^}]*)\}(?:\s*)(?:\w*?)(?:\s*)(\w*);")
-ENUM_REGEX = re.compile(r"(?<=typedef enum)(?:[\s\w]*)\{(?P<contents>[^}]*)\}(?:\s*)(?:\w*?)(?:\s*)(\w*);")
+
+# -------------------------- Source transformation -----------------------------
+CONST_REGEX = re.compile(r"#define\s+(?P<name>[a-zA-Z]\w*)\s+\(?(?P<value>[\w\d&|<>+\-*/\"]*)[\s\)]", re.MULTILINE)
+STRUCT_REGEX = re.compile(r"(?<=typedef struct)(?:[\s\w]*)\{(?P<contents>[^}]*)\}(?:\s*)(?:\w*?)(?:\s*)(?P<name>\w*);")
+ENUM_REGEX = re.compile(r"(?<=typedef enum)(?:[\s\w]*)\{(?P<contents>[^}]*)\}(?:\s*)(?:\w*?)(?:\s*)(?P<name>\w*);")
 FUNCTION_REGEX = re.compile(r"(?<=extern DECLSPEC )(?P<return_type>[\w\s\*]*)(?:SDLCALL)\s*(?P<name>[\w\d]*)\((?P<params>[\w\d\s,\*]*)\);")
+
+def sanitize(source):
+    return source.replace("@{", "").replace("@}", "")
+
+def extract_defines(source):
+    result = ""
+    matches = [m.groupdict() for m in CONST_REGEX.finditer(source)]
+    for match in matches:
+        if not match["value"]:
+            continue
+        const_type = "int"
+        if "\"" in match["value"]:
+            const_type = "string"
+        result += "public const " + const_type + " " + match["name"] + " = "
+        result += match["value"] + ";\n"
+    return result + "\n"
+
+
+def extract_structs(source):
+    result = ""
+    matches = [m.groupdict() for m in STRUCT_REGEX.finditer(source)]
+    for match in matches:
+        result += "public struct " + match["name"] + "\n{\n"
+        result += match["contents"]
+        result += "\n}\n"
+    return result + "\n"
+
+def extract_enums(source):
+    result = ""
+    matches = [m.groupdict() for m in ENUM_REGEX.finditer(source)]
+    for match in matches:
+        result += "public enum " + match["name"] + "\n{\n"
+        result += match["contents"]
+        result += "\n}\n"
+    return result + "\n"
+
+def extract_functions(source):
+    result = ""
+    matches = [m.groupdict() for m in FUNCTION_REGEX.finditer(source)]
+    for match in matches:
+        result += "[DllImport]\n"
+        result += "public static extern " + match["return_type"] + " " + match["name"]
+        result += "(" + match["params"] + ");\n"
+    return result + "\n"
+
+
+def build_source(class_name, contents):
+    contents = sanitize(contents)
+    result = "using System;\n\nnamespace SDL2\n{\npublic static class "
+    result += class_name
+    result += "\n{\n"
+    result += extract_defines(contents)
+    result += extract_enums(contents)
+    result += extract_structs(contents)
+    result += extract_functions(contents)
+    result += "}\n}\n"
+    return result
+
+# ------------------------------------------------------------------------------
+
+
 
 os.makedirs(out_folder, exist_ok=True)
 for (header, path) in headers:
@@ -44,5 +108,6 @@ for (header, path) in headers:
     contents = header_file.read()
     header_file.close()
     source_file = open(os.path.join(out_folder, header[:-2] + ".cs"), mode="w")
-    # TODO
+    source_file.write(build_source(header[:-2], contents))
     source_file.close()
+
